43e0fc69c6ec9ff0052460ac1e7285d168c41805
Vasilis van Gemert <github@vasilis.nl>
2019-04-04 22:51:19 +0200

    First scene done

--- yt.js
+++ yt.js
@@ -69,7 +69,47 @@ function updateTimerDisplay(){
 	pTimes(17,42.8,43.1,t);
 	pTimes(18,43.3,44.1,t);
 	//Interlinked
-	
+	pTimes(19,44,44.5,t);
+	pTimes(20,45,45.7,t);
+	//What's it like to hold the hand of someone you love?
+	pTimes(21,45.7,47.6,t);
+	pTimes(22,47.6,48.1,t);
+	pTimes(23,48.3,49,t);
+	//Did they teach you how to feel, finger to finger?
+	pTimes(24,49.1,50.9,t);
+	pTimes(25,50.9,51.4,t);
+	pTimes(26,51.6,52.3,t);
+	//Do you long for having your heart interlinked?
+	pTimes(27,52.3,54,t);
+	pTimes(28,54,54.5,t);
+	pTimes(29,54.7,55.4,t);
+	//Do you dream about being interlinked?
+	pTimes(30,55.4,57.2,t);
+	pTimes(31,57.2,58,t);
+	//What's it like to hold your child in your arms?
+	pTimes(32,58,59.8,t);
+	pTimes(33,59.8,60.3,t);
+	pTimes(34,60.3,61,t);
+	//Do you feel that there's a part of you that's missing?
+	pTimes(35,61,62.7,t);
+	pTimes(36,62.7,63.2,t);
+	pTimes(37,63.2,63.9,t);
+	//Within cells interlinked.
+	pTimes(38,63.9,65.2,t);
+	pTimes(39,65.2,66.6,t);
+	//Why don't you say that three times: "within cells interlinked".
+	pTimes(40,66.6,69.1,t);
+
+	pTimes(41,69.1,70.6,t);
+	pTimes(42,70.6,72.2,t);
+	pTimes(43,72.2,74.2,t);
+	// We're done.
+	pTimes(44,77.1,78.7,t);
+	pTimes(45,80,81.738,t);
+	pTimes(46,82.2,84.3,t);
+	pTimes(47,85.4,87,t);
+
+
 	if ( t < 136.1) {
 		setTimeout(() => {
 			updateTimerDisplay();
