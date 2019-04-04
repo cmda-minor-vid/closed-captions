fe70e9ba01a0292b44996987322d785b4ba8a828
Vasilis van Gemert <github@vasilis.nl>
2019-04-04 11:45:06 +0200

    First 43 seconds done, spans and classes added

--- yt.js
+++ yt.js
@@ -17,28 +17,65 @@ function onYouTubeIframeAPIReady() {
 }
 
 function initialize(){
-	var time_update_interval;
 	// Update the controls on load
+	addSpans();
+}
+function addSpans(){
+	var ps = document.querySelectorAll('#closed-captions p');
+	var i = 0;
+	var regex = /\S+/g;
+	while ( i < ps.length ) {
+		var str = ps[i].innerText;
+		var result = str.replace(regex, function(a) {
+			return "<span>" + a + "</span>";
+		});
+		ps[i].innerHTML = result;
+		ps[i].classList.add('p' + i);
+		i++;
+	}
 	updateTimerDisplay();
-	//updateProgressBar();
-
-	// Clear any old interval.
-	clearInterval(time_update_interval);
-	
-	// Start interval to update elapsed time display and
-	// the elapsed part of the progress bar every second.
-	time_update_interval = setInterval(function () {
-		updateTimerDisplay();
-		//updateProgressBar();
-	}, 100)
-
 }
 
 function updateTimerDisplay(){
 	var t = player.getCurrentTime();
 	t = Math.floor10(t,-1);
-	pTimes(1,3.8,8.1,t);
-	pTimes(2,8.1,10,t);
+	// for each paragraph we want to know:
+	// (paragraph number, start time, end time, current time)
+
+	//Officer K D 6 - 3 . 7. Let’s begin. Ready?
+	pTimes(0,3.8,8.1,t);
+	pTimes(1,8.1,10,t);
+	pTimes(2,10.8,12.5,t);
+	//And blood-black nothingness began to spin
+	pTimes(3,12.5,15.6,t);
+	pTimes(4,17.369,20,t);
+	pTimes(5,20.1,23.3,t);
+	//Fuck off, skin-job!
+	pTimes(6,23.3,25.2,t);
+	
+	pTimes(7,26.8,31.6,t);
+	pTimes(8,32.7,33,t);
+	pTimes(9,33.2,34,t);
+	//Have you ever been in an institution?
+	pTimes(10,34,36,t);
+	pTimes(11,36,36.3,t);
+	pTimes(12,36.5,37.3,t);
+	//Do they keep you in a cell?
+	pTimes(13,37.3,38.6,t);
+	pTimes(14,38.6,38.9,t);
+	pTimes(15,39.1,39.9,t);
+	//When you're not performing your duties, do they keep you in a little box?
+	pTimes(16,39.9,42.8,t);
+	pTimes(17,42.8,43.1,t);
+	pTimes(18,43.3,44.1,t);
+	//Interlinked
+	
+	if ( t < 136.1) {
+		setTimeout(() => {
+			updateTimerDisplay();
+		}, 100);
+	}
+	
 }
 function pTimes(num,startT,endT,curT) {
 	var curP = document.querySelector('.p' + num);
